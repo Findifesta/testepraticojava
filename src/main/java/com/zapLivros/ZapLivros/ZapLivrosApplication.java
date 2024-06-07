8398c198d7768a58ed45b4d70bb896f364c2b78f
Sérgio da Rocha <113067613+Findifesta@users.noreply.github.com>
2024-06-07 20:05:53 -0300

    Add files via upload

---
+++ src/main/java/com/zapLivros/ZapLivros/ZapLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.zapLivros.ZapLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zapLivros/ZapLivros/ZapLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zapLivros.ZapLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
